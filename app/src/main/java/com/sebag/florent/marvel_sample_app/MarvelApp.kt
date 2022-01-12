55aff3885082cf41f4f01c695e5d31d8839293f9
Sebag <octhave@gmail.com>
2022-01-12 11:46:36 +0100

    Add hostactivity on presenter

---
+++ app/src/main/java/com/sebag/florent/marvel_sample_app/MarvelApp.kt
@@ -0,0 +1,11 @@
+package com.sebag.florent.marvel_sample_app
+
+import android.app.Application
+
+class MarvelApp : Application() {
+
+    @Override
+    override fun onCreate() {
+        super.onCreate()
+    }
+}

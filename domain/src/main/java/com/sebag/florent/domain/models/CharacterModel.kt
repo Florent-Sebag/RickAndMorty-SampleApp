158cc31390918d6c6fcab85bb52aad1cc4f8d12c
Sebag <octhave@gmail.com>
2022-01-13 15:26:46 +0100

    Upgrade front

--- domain/src/main/java/com/sebag/florent/domain/models/CharacterModel.kt
+++ domain/src/main/java/com/sebag/florent/domain/models/CharacterModel.kt
@@ -3,5 +3,17 @@ package com.sebag.florent.domain.models
 data class CharacterModel(
     val id : Int,
     val name : String,
-    val description : String
+    val description : String,
+    val thumbnail: Thumbnail
 )
+
+data class Thumbnail(
+    val path : String,
+    val extension: String,
+) {
+    var concatedPath: String? = null
+        get() {
+            field = field ?:  "$path.$extension"
+            return field
+        }
+}
--- presenter/src/main/java/com/sebag/florent/presenter/view/home/CharacterAdapter.kt
+++ presenter/src/main/java/com/sebag/florent/presenter/view/home/CharacterAdapter.kt
@@ -2,14 +2,26 @@ package com.sebag.florent.presenter.view.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sebag.florent.domain.models.CharacterModel
 import com.sebag.florent.presenter.databinding.ItemCharacterBinding
 
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    if (url != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") {
+        Glide.with(view.context).load(url).into(view)
+    }
+}
+
 class CharacterAdapter :
     PagingDataAdapter<CharacterModel, CharacterAdapter.CharacterViewHolder>(CharacterComparator) {
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         CharacterViewHolder(
